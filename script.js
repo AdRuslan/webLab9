b9d456040f59293528fa6d51294edc8f800bf410
AdRuslan <russian.adig@mail.ru>
2021-01-07 02:14:30 +0500

    task4

--- script.js
+++ script.js
@@ -45,16 +45,39 @@
 // arrayCut([1, 2, 3, 4, 5]);
 
 //Task3
-function isPalindrome(word) {
-  let firstPart = word.slice(0, Math.floor(word.length / 2));
-  let secondPart = word
-    .slice(Math.ceil(word.length / 2))
-    .split("")
-    .reverse()
-    .join("");
+// function isPalindrome(word) {
+//   let firstPart = word.slice(0, Math.floor(word.length / 2));
+//   let secondPart = word
+//     .slice(Math.ceil(word.length / 2))
+//     .split("")
+//     .reverse()
+//     .join("");
 
-  return firstPart == secondPart ? true : false;
+//   return firstPart == secondPart ? true : false;
+// }
+
+// console.log(isPalindrome("довод"));
+// console.log(isPalindrome("кружка"));
+
+//Task4
+function scalarMultiplication(mltplr, matr) {
+  let newMatr = [];
+
+  for (let i of matr) {
+    newMatr.push(i.map((item) => (item *= mltplr)));
+  }
+
+  let printMatr = function (matr) {
+    for (let i of matr) {
+      console.log(i.join("  "));
+    }
+  };
+
+  printMatr(newMatr);
 }
 
-console.log(isPalindrome("довод"));
-console.log(isPalindrome("кружка"));
+scalarMultiplication(5, [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
