9184b5de0d985ee6a391f2b0deccfc6808db7083
AdRuslan <russian.adig@mail.ru>
2021-01-07 01:04:34 +0500

    task2

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 "use strict";
 
 //Task1
-function User(name, age, group) {
-  this.name = name;
-  this.age = age;
-  this.group = group;
-  this.isAdmin = false;
+// function User(name, age, group) {
+//   this.name = name;
+//   this.age = age;
+//   this.group = group;
+//   this.isAdmin = false;
 
-  this.propList = function () {
-    let properties = [];
+//   this.propList = function () {
+//     let properties = [];
 
-    for (let prop in this) {
-      properties.push(prop);
-    }
+//     for (let prop in this) {
+//       properties.push(prop);
+//     }
 
-    alert(`Список доступных свойств и методов: ${properties.join(", ")}`);
-  };
+//     alert(`Список доступных свойств и методов: ${properties.join(", ")}`);
+//   };
 
-  this.userInf = function () {
-    alert(
-      `Студент по имени ${this.name} имеет возраст - ${
-        this.age
-      }, учится в группе - ${this.group}, статус администратора ${
-        this.isAdmin === false ? "не имеет" : "имеет"
-      }.`
-    );
-  };
+//   this.userInf = function () {
+//     alert(
+//       `Студент по имени ${this.name} имеет возраст - ${
+//         this.age
+//       }, учится в группе - ${this.group}, статус администратора ${
+//         this.isAdmin === false ? "не имеет" : "имеет"
+//       }.`
+//     );
+//   };
+// }
+
+// let Masha = new User("Маша", 21, 201);
+
+// Masha.propList();
+// Masha.userInf();
+
+//Task 2
+function arrayCut(arr) {
+  while (arr.length > 1) {
+    console.log(`${arr.pop()} осталось [${arr}]`);
+  }
+
+  console.log(`${arr[0]}`);
 }
 
-let Masha = new User("Маша", 21, 201);
-
-Masha.propList();
-Masha.userInf();
-
-
-
+arrayCut([1, 2, 3, 4, 5]);
